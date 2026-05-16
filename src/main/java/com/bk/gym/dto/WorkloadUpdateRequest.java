074f433ab850047ac2235c94782e0f9c4e9170ac
Bidzina Kevkhishvili <bk2451@nyu.edu>
2026-05-17 01:57:10 +0400

    Messaging using ActiveMQ

--- src/main/java/com/bk/gym/dto/WorkloadUpdateRequest.java
+++ src/main/java/com/bk/gym/dto/WorkloadUpdateRequest.java
@@ -1,36 +1,21 @@
 package com.bk.gym.dto;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-@ApiModel("Workload Update Request")
 @Getter
 @Setter
-public class WorkloadUpdateRequest {
+public class WorkloadUpdateRequest implements Serializable {
 
-    @ApiModelProperty(required = true)
     private String trainerUsername;
-
-    @ApiModelProperty(required = true)
     private String firstName;
-
-    @ApiModelProperty(required = true)
     private String lastName;
-
-    @ApiModelProperty(required = true)
     private boolean isActive;
-
-    @ApiModelProperty(required = true, example = "2024-05-01")
     private LocalDate trainingDate;
-
-    @ApiModelProperty(required = true, example = "60")
     private int trainingDuration;
-
-    @ApiModelProperty(required = true, allowableValues = "ADD,DELETE")
     private String actionType;
 
 }
