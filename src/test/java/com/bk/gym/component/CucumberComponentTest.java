70eb6f492693f08dc1f6512c2176259bb90130ea
Bidzina Kevkhishvili <bk2451@nyu.edu>
2026-06-02 22:31:11 +0400

    Adding dockerfile and changing application.properties

--- src/test/java/com/bk/gym/component/CucumberComponentTest.java
+++ src/test/java/com/bk/gym/component/CucumberComponentTest.java
@@ -1,8 +1,18 @@
 package com.bk.gym.component;
 
+import com.bk.gym.GymApp;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:features",
+        glue = "com.bk.gym.component"
+)
+@SpringBootTest(classes = GymApp.class)
 public class CucumberComponentTest {}
